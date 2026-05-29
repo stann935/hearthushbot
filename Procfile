1e2864d7d8408bf7f7277c02e9da307884eee11e
stann935 <stanfordlorenzo799@gmail.com>
2026-05-29 17:08:37 +0300

    Fix Render startup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python main.py
+worker: python render_start.py
---
+++ render_start.py
@@ -0,0 +1,15 @@
+import asyncio
+import sys
+from main import main
+
+if __name__ == "__main__":
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.close()
+
